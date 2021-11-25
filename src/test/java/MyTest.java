e70fb87212137db3de22f72d42d2d9550160dd7e
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-11-25 20:21:53 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,20 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void FixedTests() {
+        assertEquals(18, Solution.century(1705));
+        assertEquals(19, Solution.century(1900));
+        assertEquals(17, Solution.century(1601));
+        assertEquals(20, Solution.century(2000));
+        assertEquals(1,  Solution.century(89));
+    }
 
+    private static class Solution {
+        public static int century(int number) {
+            return (number + 99) / 100;
+        }
     }
 }
